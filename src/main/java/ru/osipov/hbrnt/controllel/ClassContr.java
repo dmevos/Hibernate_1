fac20fb4d033cc454feb67c3ae45f098f1f6ccce
Dmitry Osipov <deosipov@mail.ru>
2023-03-17 10:01:40 +0300

    Задание2 DAO c JPA Repositories работает через Query

--- src/main/java/ru/osipov/hbrnt/controllel/ClassContr.java
+++ src/main/java/ru/osipov/hbrnt/controllel/ClassContr.java
@@ -3,7 +3,6 @@ package ru.osipov.hbrnt.controllel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.osipov.hbrnt.model.ComposeKey;
 import ru.osipov.hbrnt.model.Persons;
 import ru.osipov.hbrnt.repository.MyCrudRepository;
 
@@ -19,33 +18,18 @@ public class ClassContr {
         this.rep = rep;
     }
 
-    @GetMapping("persons/add") //http://localhost:8080/persons/add
-    public Persons mySave() { //Лень передать через @RequestParam пэтому буду удалять в теле сам
-        return rep.save(
-                new Persons(
-                        new ComposeKey("Dima3", "Dimych3", 35)
-                        , "phone3"
-                        , "Yaroslavl")
-        );
-    }
-
-    @GetMapping("persons/del") //http://localhost:8080/persons/del
-    public void myDel() { //Лень передать ComposeKey через @RequestParam пэтому буду удалять в теле сам
-        rep.deleteById(new ComposeKey("Dima3", "Dimych3", 35));
-    }
-
     @GetMapping("persons/by-city")  //http://localhost:8080/persons/by-city?city=Kostroma
     public List<Persons> myListPersonsByCity(@RequestParam String city) {
-        return rep.findAllByCityOfLiving(city);
+        return rep.getAllByCityOfLivingQuery(city);
     }
 
     @GetMapping("persons/by-age")  //http://localhost:8080/persons/by-age?age=29
     public List<Persons> myListPersonsByAge(@RequestParam String age){
-        return rep.findAllByComposeKeyAgeLessThanOrderByComposeKeyAge(Integer.parseInt(age));
+        return rep.getAllByAgeLessThanOrderByAgeQuery(Integer.parseInt(age));
     }
 
     @GetMapping("persons/by-name_surname")  //http://localhost:8080/persons/by-name_surname?name=Dima3&surname=Dimych3
     public Optional<Persons> myListPersonsByAge(@RequestParam String name, @RequestParam String surname){
-        return rep.findByComposeKeyNameAndComposeKeySurname(name, surname);
+        return rep.getByNameAndSurnameJPQLQuery(name, surname);
     }
 }
--- src/main/java/ru/osipov/hbrnt/repository/MyCrudRepository.java
+++ src/main/java/ru/osipov/hbrnt/repository/MyCrudRepository.java
@@ -1,5 +1,6 @@
 package ru.osipov.hbrnt.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.osipov.hbrnt.model.ComposeKey;
 import ru.osipov.hbrnt.model.Persons;
@@ -9,7 +10,12 @@ import java.util.Optional;
 
 public interface MyCrudRepository extends CrudRepository<Persons, ComposeKey> {
 
-    List<Persons> findAllByCityOfLiving(String cityOfLiving);
-    List<Persons> findAllByComposeKeyAgeLessThanOrderByComposeKeyAge(int age);
-    Optional<Persons> findByComposeKeyNameAndComposeKeySurname(String name, String surname);
+    @Query(value = "select * from persons where city_of_living like ?1", nativeQuery = true)
+    List<Persons> getAllByCityOfLivingQuery(String cityOfLiving);
+
+    @Query(value = "select * from persons where age<?1 order by age", nativeQuery = true)
+    List<Persons> getAllByAgeLessThanOrderByAgeQuery(int age);
+
+    @Query("select p from Persons p where p.composeKey.name = :name and p.composeKey.surname = :surname")
+    Optional<Persons> getByNameAndSurnameJPQLQuery(String name, String surname);
 }
